ea632865ddc5396d44c6f690257b43a22a7accb8
Ezequiel Lovelle <ezequiellovelle@gmail.com>
2014-09-08 14:16:59 -0300

    first commit

---
+++ config.js
@@ -0,0 +1,91 @@
+var conf =
+{
+    domain: 'domain.ltd',
+    server_type: 'socket.io',
+    server: 'localhost',
+    port: '3000',
+    debug: true,
+    auto_login: false,
+    sound_active: true,
+    login_popup: true,
+    tools_disabled: false,
+    tools:
+    {
+        icon: 'ui-icon-wrench'
+    },
+
+    options_disabled: false,
+    options:
+    {
+        icon: 'ui-icon-triangle-1-n'
+    },
+
+    bar:
+    {
+        default_expand: true,
+        icon_expand: 'ui-icon-arrowthickstop-1-e',
+        icon_collapse: 'ui-icon-arrowthickstop-1-w'
+    },
+
+    theme_default: 'smoothness',
+    themes:
+    [
+        { name: 'black-tie' },
+        { name: 'blitzer' },
+        { name: 'cupertino' },
+        { name: 'dark-hive' },
+        { name: 'dot-luv' },
+        { name: 'eggplant' },
+        { name: 'excite-bike' },
+        { name: 'flick' },
+        { name: 'hot-sneaks' },
+        { name: 'humanity' },
+        { name: 'le-frog' },
+        { name: 'mint-choc' },
+        { name: 'overcast' },
+        { name: 'pepper-grinder' },
+        { name: 'redmond' },
+        { name: 'south-street' },
+        { name: 'start' },
+        { name: 'sunny' },
+        { name: 'swanky-purse' },
+        { name: 'trontastic' },
+        { name: 'ui-darkness' },
+        { name: 'ui-lightness' },
+        { name: 'vader' }
+    ],
+
+    lang:
+    [
+        {
+            text: 'Spanish',
+            i18n: 'es'
+        },
+        {
+            text: 'English',
+            i18n: 'en'
+        }
+    ],
+
+    shortcuts:
+    [
+        {
+            text: 'Home',
+            href: 'https://github.com/lovelle/jquery-chat/',
+            icon: 'ui-icon-home',
+            target: '_blank'
+        },
+        {
+            text: 'Mail',
+            href: 'https://gmail.com/',
+            icon: 'ui-icon-mail-closed',
+            target: '_blank'
+        },
+        {
+            text: 'Search',
+            href: 'https://google.com/',
+            icon: 'ui-icon-search',
+            target: '_blank'
+        }
+    ]
+}
---
+++ server/server.js
@@ -0,0 +1,67 @@
+var port = 3000, //Listen port
+server   = require('http').createServer(),
+io       = require('socket.io').listen(server),
+
+users = {}, socks = {};
+
+function Uid() { this.id = ++Uid.lastid; }
+
+Uid.lastid = 0;
+
+//Handle users
+io.sockets.on('connection', function (socket) {
+
+	// Event received by new user
+	socket.on('join', function (recv) {
+
+		if (!recv.user) {
+			socket.emit('custom_error', { message: 'User not found or invalid' });
+			return;
+		}
+
+		// The user already exists
+		if (users[recv.user]) {
+			socket.emit('custom_error', { message: 'The user '+ recv.user +' already exists' });
+			return;
+		}
+
+		// If there is users online, send the list of them
+		if (Object.keys(users).length > 0)
+			socket.emit('chat', JSON.stringify( { 'action': 'usrlist', 'user': users } ));
+
+		// Set new uid
+		uid = new Uid();
+		socket.user = recv.user;
+
+		// Add the new data user
+		users[socket.user] = {'uid': Uid.lastid, 'user': socket.user, 'name': recv.name, 'status': 'online'}
+		socks[socket.user] = {'socket': socket}
+
+		// Send new user is connected to everyone
+		socket.broadcast.emit('chat', JSON.stringify( {'action': 'newuser', 'user': users[socket.user]} ));
+	});
+
+	// Event received when user want change his status
+	socket.on('user_status', function (recv) {
+		users[socket.user].status = recv.status;
+		socket.broadcast.emit('chat', JSON.stringify( {'action': 'user_status', 'user': users[socket.user]} ));
+	});
+
+	// Event received when user send message to another
+	socket.on('message', function (recv) {
+		var id  = socks[recv.user].socket.id;
+		var msg = {'msg': recv.msg, 'user': users[socket.user]};
+		io.sockets.socket(id).emit('chat', JSON.stringify( {'action': 'message', 'data': msg} ));
+	});
+
+	// Event received when user has disconnected
+	socket.on('disconnect', function () {
+		socket.broadcast.emit('chat', JSON.stringify( {'action': 'disconnect', 'user': users[socket.user]} ));
+		//socket.broadcast.emit('chat', JSON.stringify( {'action': 'offline', 'user': users[socket.user]} ));
+		delete users[socket.user];
+		delete socks[socket.user];
+	});
+});
+
+//Listen to the server port
+server.listen(port)
